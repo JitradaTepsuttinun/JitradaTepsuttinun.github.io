c2e4f79dc22acb136f658a5384f26657881fcd5c
JitradaTepsuttinun <u6410725@au.edu>
2024-07-10 13:04:10 +0700

    update react-vat-calculator

--- react-vat-calculator/src/App.jsx
+++ react-vat-calculator/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [a, setA] = useState(9)
+  const [a, setA] = useState(0)
   const [vat, setVAT] = useState(0)
   const [discount, setDiscount] = useState(0)
   // 1. Add a variable b,
@@ -20,32 +20,35 @@ function App() {
     console.log(discount)
   }
 
-  const handler = (e) => {
-    const price = e.target.value
-    console.log(price)
-    setA(price)
-    let v = (price - discount) * 0.07
-    setVAT(Math.round(v * 100) / 100)
+  // const handler = (e) => {
+  //   const price = e.target.value
+  //   console.log(price)
+  //   setA(price)
+  //   setDiscount(discount)
+  //   let v = (price - discount) * 0.07
+  //   setVAT(Math.round(v * 100) / 100)
+  // }
+  function calculate() {
+    let v = (a - discount) * 0.07
+    setVAT(Math.round(v*100)/100)
   }
 
   return (
     <>
     <input type="number" value={a}
       style={ {fontSize: '30pt'} }
-      onChange={handler}/>
+      onChange={addA}/>
     <br></br>
     <input type="number" value={discount}
       style={ {fontSize: '30pt'} }
-      onChange={handler} />
-
+      onChange={addDiscount} />
+    <br></br>
+    <button type='button' onClick={calculate}>Calculate</button>
       <h1>My A = {a}</h1>
       <h1>My discount = {discount}</h1>
       <h1>VAT = {vat}</h1>
       <div className="card">
 
-
-        <button onClick={addA}>Increase A</button>
-        <button onClick={addDiscount}>Increase Discount</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
